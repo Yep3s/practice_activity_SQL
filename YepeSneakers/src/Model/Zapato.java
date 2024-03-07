db6e95d70ee4bc8f16dbe07062ee3222328e0927
Yep3s <contactosebastianyepes@gmail.com>
2024-03-06 20:47:28 -0500

    se crea el modelo de zapato con getters y setters

---
+++ YepeSneakers/src/Model/Zapato.java
@@ -0,0 +1,54 @@
+
+package Model;
+
+
+public class Zapato {
+    
+    private String marca;
+    private String talla;
+    private String modelo;
+    private String color;
+
+    public Zapato(String marca, String talla, String modelo, String color) {
+        this.marca = marca;
+        this.talla = talla;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+    
+    
+}
