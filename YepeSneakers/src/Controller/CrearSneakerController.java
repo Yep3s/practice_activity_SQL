83d5cf00744826f9ad8b7c6a2821492ea87e9394
Yep3s <contactosebastianyepes@gmail.com>
2024-03-07 23:08:25 -0500

    se sigue trabajando en la vista de crear sneaker ya casi finalizando

--- YepeSneakers/src/Controller/CrearSneakerController.java
+++ YepeSneakers/src/Controller/CrearSneakerController.java
@@ -3,11 +3,27 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CrearSneakerController {
 
     @FXML
+    private TextField brand;
+
+    @FXML
+    private TextField color;
+
+    @FXML
     private Button crearNuevoSneaker;
+
+    @FXML
+    private TextField model;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TextField size;
 
     @FXML
     void nuevoSneakerBtn(ActionEvent event) {
